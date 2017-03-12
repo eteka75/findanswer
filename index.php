e7f85d4fe1f6c16833cc0e305e3382ae0ce94853
eteka <etekawilfried@gmail.com>
2017-03-12 19:20:23 +0100

    Version 1.2 de l'Application

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include './includes/header.php';
 
 //$db = connect(); 
 print_r($db);
-$pdo= new PDO('sqlite:/lib/qr.db');
+$pdo= new PDO('sqlite:./db/qr.db');
 print_r($pdo);
 /* Connexion à la base de donnée */
 //$entreprises = $db->select("entreprises", '1', 'ORDER BY created_at');
